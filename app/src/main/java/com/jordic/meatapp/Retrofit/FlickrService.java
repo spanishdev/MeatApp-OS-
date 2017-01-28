b7a33a6754526d479244dd7fb8d08a22cdcb9d96
Jordi <spanishdev@gmail.com>
2017-01-29 00:53:59 +0100

    Changed flickr service

--- app/src/main/java/com/jordic/meatapp/Retrofit/FlickrService.java
+++ app/src/main/java/com/jordic/meatapp/Retrofit/FlickrService.java
@@ -11,8 +11,7 @@ import retrofit2.http.Query;
  */
 
 public interface FlickrService {
-//api_sig=0aa288cbcfc9d5cd413e66bde4062cf9
-    //&api_key=e9e5b255ec66dac866c68ee8257f3553
+
     @GET("services/rest/?method=flickr.photos.getSizes&format=json&nojsoncallback=1")
     Call<FlickrPOJO> getPhotoUrls(@Query("photo_id") String photo_id, @Query("api_key") String api_key, @Query("api_sig") String api_sig);
 
